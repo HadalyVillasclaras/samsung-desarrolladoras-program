01fa50cd063724e07b358f6680e8bd9a7948516d
Hadaly <hadalyvf@gmail.com>
2023-05-21 20:49:21 +0200

    feat: final lab structure folder

---
+++ b4-final-lab/js/main.js
@@ -0,0 +1,72 @@
+const formFeedback = document.getElementById('form-feedback');
+const inputFields = document.querySelectorAll('.input-field');
+const form = document.querySelector('form');
+
+form.addEventListener("submit", event => handleSubmit(event));
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  if (validateFields(inputFields)) {
+    submitForm(event);
+  }
+}
+
+
+function validateFields(inputFields) {
+
+  let isValid = true;
+
+  for (let i = 0; i < inputFields.length; i++) {
+    let input = inputFields[i];
+    let errorMessage = input.nextElementSibling;
+
+    if (!input.value.trim()) {
+      isValid = false;
+      errorMessage.textContent = 'Este campo es requerido.';
+      errorMessage.style.display = 'block';
+    } else if (input.name === 'password' && (input.value.length < 4 || input.value.length > 8)) {
+      isValid = false;
+      errorMessage.textContent = 'La contraseña debe tener entre 4 y 8 caracteres.';
+      errorMessage.style.display = 'block';
+    } else if (input.name === 'email' && !input.value.match(/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/)) {
+      isValid = false;
+      errorMessage.textContent = 'Introduzca un correo electrónico válido.';
+      errorMessage.style.display = 'block';
+    } else {
+      errorMessage.textContent = '';
+      errorMessage.style.display = 'none';
+    }
+  }
+
+  return isValid;
+}
+
+function submitForm(event) {
+  event.preventDefault();
+
+  let formData = new FormData(event.target);
+
+  fetch('./src/register.php', {
+    method: 'POST',
+    body: formData
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw Error(response.status);
+    }
+    return response.text();
+  })
+  .then(data => {
+    formFeedback.textContent = 'Registro completado con éxito';
+  })
+  .catch((error) => {
+    if (error.message == '409') {
+      formFeedback.textContent = 'El correo electrónico ya está registrado.';
+    } else {
+      console.error('Error:', error);
+    }
+  });
+}
+
+
